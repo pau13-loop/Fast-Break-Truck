a423a7df0a321aa982300a2558cf3558dcf370eb
pau <pllinas@cifpfbmoll.eu>
2021-05-02 20:39:54 +0200

    feat: :sparkles: Set directory structure

---
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/App.java
@@ -0,0 +1,13 @@
+package edu.pingpong.fastbreaktruck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
