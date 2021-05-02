1c698eb707d3c08d2595e96708f9c9a3a77d5a08
pau <pllinas@cifpfbmoll.eu>
2021-05-02 20:41:14 +0200

    feat: :sparkles: Add user stories

--- fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/App.java
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/App.java
@@ -1,13 +1,35 @@
 package edu.pingpong.fastbreaktruck;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Desayuno Eyeholes
+		 */
+
+	// 	Desayuno eyeDes = FastBreakTruck.prepararEyeHoles();
+	// 	System.out.println("\nNo abras la caja de Eyeholes!");
+	// 	eyeDes.mostrarItems();
+	// 	System.out.print("Precio pedido: " + eyeDes.getCoste());
+
+	// 	/**
+	// 	 * Desayuno Smiggles
+	// 	 */
+		
+	// 	Desayuno smigDes = FastBreakTruck.prepararSmiggles();
+	// 	System.out.println("\nSmiggles per als nins!");
+	// 	smigDes.mostrarItems();
+	// 	System.out.print("Precio pedido: " + smigDes.getCoste());
+
+	// 	/**
+	// 	 * Plumbus
+	// 	 */
+
+	// 	eyeDes = FastBreakTruck.prepararEyeHoles();
+	// 	FastBreakTruck.incluirJuguete(eyeDes);
+	// 	System.out.println("\nEyeholes con plumbus!");
+	// 	eyeDes.mostrarItems();
+	// 	System.out.print("Precio pedido: " + eyeDes.getCoste());
+	// }
 }
