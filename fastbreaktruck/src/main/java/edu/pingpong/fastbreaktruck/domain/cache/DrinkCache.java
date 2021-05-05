d908db40e1fdf200568ec0f3173d858826675302
pau <pllinas@cifpfbmoll.eu>
2021-05-05 21:45:29 +0200

    feat: :sparkles: Add drink cache

---
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/domain/cache/DrinkCache.java
@@ -0,0 +1,30 @@
+package edu.pingpong.fastbreaktruck.domain.cache;
+
+import java.util.Hashtable;
+
+import edu.pingpong.fastbreaktruck.domain.drink.Drink;
+import edu.pingpong.fastbreaktruck.domain.drink.FleebJuice;
+import edu.pingpong.fastbreaktruck.domain.drink.TurbulentJuice;
+
+public class DrinkCache {
+
+    // Create map with all kinds of cereals
+    private static Hashtable<String, Drink> drinksMap = new Hashtable<>();
+
+    // Get cereal object from map and make a clone of it
+    public static Drink getDrinks(String drinkId) {
+        Drink cachedDrink = drinksMap.get(drinkId);
+        return (Drink) cachedDrink.clone();
+    }
+
+        // Create cereal objects and add the objects to the dictionary to afterwards be able to make a copy of them
+        public static void loadCache() {
+            FleebJuice fleebjuice = new FleebJuice();
+            fleebjuice.setId("fleeb");
+            drinksMap.put(fleebjuice.getId(), fleebjuice);
+    
+            TurbulentJuice turbulentJuice = new TurbulentJuice();
+            turbulentJuice.setId("turbulent");
+            drinksMap.put(turbulentJuice.getId(), turbulentJuice);
+        }
+}
