92eaf5df9cc0e53fe85fd9c85ccb2eced18dee78
pau <pllinas@cifpfbmoll.eu>
2021-05-05 22:21:35 +0200

    feat: :sparkles: Add second user story

--- fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/main/App.java
+++ fastbreaktruck/src/main/java/edu/pingpong/fastbreaktruck/main/App.java
@@ -1,23 +1,52 @@
 package edu.pingpong.fastbreaktruck.main;
 
 import edu.pingpong.fastbreaktruck.domain.cache.CerealsCache;
+import edu.pingpong.fastbreaktruck.domain.cache.DrinkCache;
+import edu.pingpong.fastbreaktruck.domain.cache.EnvelopCache;
 import edu.pingpong.fastbreaktruck.domain.cereals.Cereals;
+import edu.pingpong.fastbreaktruck.domain.drink.Drink;
+import edu.pingpong.fastbreaktruck.domain.envelop.Envelop;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		// Primera Prueba 
+		// Load prototipes
 		CerealsCache.loadCache();
+		DrinkCache.loadCache();
+		EnvelopCache.loadCache();
 
-		// conciliatory message
+		// MENU 1 - Desayuno Eyeholes
+
+		// Conciliatory message
 		System.out.println("\nNo abras la caja de Eyeholes!");
-		// Show menu details
-		Cereals clonedCereals = (Cereals) CerealsCache.getCereals("smiggles");
-		// Cereals clonedCerealsPvp = (Cereals) CerealsCache.getCerealsPvp("eyeholes");
+		
+		// Get cloned object
+		// CEREALS
+		Cereals clonedEyes = (Cereals) CerealsCache.getCereals("eyeholes");
+		Cereals clonedSmiggles = (Cereals) CerealsCache.getCereals("smiggles");
+		// DRINKS
+		Drink clonedTurbulent = (Drink) DrinkCache.getDrinks("turbulent");
+		Drink clonedFleeb = (Drink) DrinkCache.getDrinks("fleeb");
+		// ENVELOPS
+		Envelop clonedBox = (Envelop) EnvelopCache.getEnvelop("box");
+		Envelop clonedBottle = (Envelop) EnvelopCache.getEnvelop("bottle");
 
-		System.out.println("\nItem: " + clonedCereals.getType() + "\nPrice: " + clonedCereals.getPvp());
-		System.out.println("\n\tPrecio pedido: ");
+		// Print message showing menu
+		System.out.println("\nItem: " + clonedEyes.getName() + "\nEnvelop: " + clonedBox.getName() + "\nPrice: " + clonedEyes.getPvp());
+		System.out.println("\nItem: " + clonedTurbulent.getName() + "\nEnvelop: " + clonedBottle.getName() + "\nPrice: " + clonedTurbulent.getPvp());
+		System.out.println("\n\tPrecio pedido: " + (clonedEyes.getPvp() + clonedTurbulent.getPvp()));
+
+
+		// MENU 2 - Desayuno Smiggles
+
+		// Conciliatory message
+		System.out.println("\nSmiggles per als nins!");
+
+		// Print message showing menu
+		System.out.println("\nItem: " + clonedSmiggles.getName() + "\nEnvelop: " + clonedBox.getName() + "\nPrice: " + clonedSmiggles.getPvp());
+		System.out.println("\nItem: " + clonedFleeb.getName() + "\nEnvelop: " + clonedBottle.getName() + "\nPrice: " + clonedFleeb.getPvp());
+		System.out.println("\n\tPrecio pedido: " + (clonedSmiggles.getPvp() + clonedFleeb.getPvp()));
 		
 		
 		
